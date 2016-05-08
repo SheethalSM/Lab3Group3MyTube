3f0ce51e2e4dd139410a4b229f0e0562a4883f78
Wrinth <johnl4112@gmail.com>
2016-05-08 15:34:37 -0700

    Project starting files

---
+++ app/src/main/java/com/wrinth/lab3group3mytube/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wrinth.lab3group3mytube;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
